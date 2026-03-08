36df33da049ea93d1604dbe683587e1ef26bd912
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-08 23:57:38 +0900

    mod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </nav>
         </div>
       </header>
-      <main className="mt-12.5 bg-linear-to-r from-lime-100 to-lime-200 h-screen flex flex-col items-center justify-center">
+      <main className="pt-12.5 bg-linear-to-r from-lime-100 to-lime-200 h-screen flex flex-col items-center justify-center">
         <div className="text-center">
           <h1 className="text-7xl logo">スケジュール管理APP</h1>
           <p className="pt-[10vh] text-5xl">お互いのスケジュールを管理するアプリです</p>
