886af255b78fbf922e24af98f2c9214f92c7e6e2
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-08 23:32:34 +0900

    feat: Topページを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 export default function Home() {
   return(
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="font-bold text-3xl text-lime-500 underline">Hello World</h1>
+    <div className="relative">
+      <header className="bg-white leading-12.5 fixed top-0 left-0 right-0">
+        <div className="container mx-auto flex justify-between">
+          <p className="logo">スケジュール管理APP</p>
+          <nav>
+            <ul className="flex gap-5 text-lime-800">
+              <li>利用説明</li>
+              <li>ログイン</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="mt-12.5 bg-linear-to-r from-lime-100 to-lime-200 h-screen flex flex-col items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-7xl logo">スケジュール管理APP</h1>
+          <p className="pt-[10vh] text-5xl">お互いのスケジュールを管理するアプリです</p>
+        </div>
+        <div className="pt-[20vh]">
+          <button className="bg-lime-800 text-white p-4 text-lg rounded-lg">
+            ログイン
+          </button>
+        </div>
+      </main>
     </div>
   )
 }   
