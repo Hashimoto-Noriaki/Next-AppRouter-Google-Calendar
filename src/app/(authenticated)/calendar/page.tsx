5b1b2e899573862ea2e8399622db59ff60e457df
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-10 03:17:58 +0900

    曜日ヘッターを表示

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { getMonth } from 'date-fns'
+import { DAYS_LIST } from '@/constants/calendar'
 
 export default function CalendarPage() {
     return (
@@ -8,6 +9,17 @@ export default function CalendarPage() {
             <h1 className="font-bold text-3xl mb-5">
                 {`${getMonth(new Date)+ 1}月`}
             </h1>
+            <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
+                <thead>
+                    <tr className="bg-lime-800 text-white rounded-tr-lg rounded-tl-lg py-10">
+                        {DAYS_LIST.map((day)=> (
+                            <th key={day} className="text-xl text-center py-3">
+                                {day}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+            </table>
         </>
     )
 }
