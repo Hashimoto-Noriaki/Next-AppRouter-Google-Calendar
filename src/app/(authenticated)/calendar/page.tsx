374476208a43557b5e5d4e19c998534d67e3d1b5
Hashimoto-Noriaki <nori.hasimo.89@gmail.com>
2026-03-11 20:02:05 +0900

    feat: カレンダーのナビゲーションを追加した

--- src/app/(authenticated)/calendar/page.tsx
+++ src/app/(authenticated)/calendar/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { useState,useEffect } from 'react'
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
-import { addMonths,eachDayOfInterval,eachWeekOfInterval,endOfMonth,endOfWeek,getMonth,startOfMonth } from 'date-fns'
-import { CalendarBody,CalendarHeader} from '@/features/calendar/components'
+import { eachDayOfInterval,eachWeekOfInterval,endOfMonth,endOfWeek,getMonth,startOfMonth } from 'date-fns'
+import { CalendarBody, CalendarHeader, CalendarNav } from '@/features/calendar/components'
 import type { DateList } from "@/types/calendar"
 
 export default function CalendarPage() {
@@ -31,33 +30,13 @@ export default function CalendarPage() {
         fetchCalendar()
     },[currentDate])
 
-    const changeToday = () => setCurrentDate(new Date())
-    const changePrevMonth = () =>  setCurrentDate((prev) => addMonths(prev,-1))
-    const changeNextMonth = () => setCurrentDate((prev) => addMonths(prev,1))
 
     return (
         <>
             <h1 className="font-bold text-3xl mb-5">
                 {`${getMonth(currentDate)+ 1}月`}
             </h1>
-            <div className="w-[80%] flex justify-between mb-2">
-                <div className="flex items-center gap-2">
-                    <FaArrowAltCircleLeft
-                        className="text-lime-800 text-2xl cursor-pointer"
-                        onClick={changePrevMonth}
-                    />
-                    <button
-                        className="bg-lime-800 text-white p-2 rounded-md"
-                        onClick={changeToday}
-                    >
-                        今日
-                    </button>
-                    <FaArrowAltCircleRight
-                        className="text-lime-800 text-2xl cursor-pointer"
-                        onClick={changeNextMonth}
-                    />
-                </div>
-            </div>
+            <CalendarNav setCurrentDate={setCurrentDate}/>
             <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
                 <CalendarHeader/>
                 <CalendarBody currentDate={currentDate} dateList={dateList}/>
